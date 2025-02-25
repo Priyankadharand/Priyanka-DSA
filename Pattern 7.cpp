1d8b8aaadaa59acb337c28923b53636180f69c30
Priyanka D <priyankadharanpriya@gmail.com>
2025-02-25 17:42:47 +0530

    Pattern

---
+++ Pattern 7.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"";
+    cin >> n;
+    
+    
+    for(int i=n; i>=1; --i){
+        for(int j=i; j>=1; --j){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+	// your code goes here
+    return 0;
+}
